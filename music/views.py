0d088fb43ee310d9a95ad29b56da8c52b71650ba
root <root@localhost.localdomain>
2019-05-10 11:48:22 +0530

    website project

---
+++ music/views.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+# Create your views here.
+
+from django.shortcuts import render, get_object_or_404
+from .models import Album
+
+def index(request):
+    all_albums=Album.objects.all()
+    context={
+            'all_albums': all_albums,
+    }
+    return render(request, 'music/index.html', context)
+
+def detail(request, album_id):
+    album=get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/song.html', {'album': album})
+
+def favorite(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song=album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/song.html', {
+            'album': album,
+            'error_message': "You did not select a valid song",
+        })
+    else:
+        selected_song.is_favorite= True
+        selected_song.save()
+        return render(request, 'music/song.html', {'album': album})
+
+
